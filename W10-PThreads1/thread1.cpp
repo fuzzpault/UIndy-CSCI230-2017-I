b697de32865301176b286a006a64a6966ed058a8
Paul Talaga <talagap@uindy.edu>
2017-11-01 17:49:04 +0000

    shared variable, no mutex

---
+++ W10-PThreads1/thread1.cpp
@@ -0,0 +1,69 @@
+/*                                                                                                                     
+  Name: Paul Talaga
+  Date: Oct 30, 2017
+  Desc: Program to demonstrate the use of pthreads
+        This reads and writes to a shared variable (without any semaphore or lock)
+	to show how, using threads, an incorrect result could be observed.  You
+	may need to run this multiple times to see an incorrect temp value.
+
+  To compile this, do: g++ -lpthread thread1.cpp
+*/
+
+#include <iostream>
+#include <pthread.h>
+#include <string>
+
+using namespace std;
+
+struct thread_data_t{
+  int thread_id;
+  int value;
+};
+
+int temp;
+
+void* doStuff(void* arg){
+  thread_data_t* input;
+  input = (thread_data_t*)arg;
+
+  int thread_num = input->thread_id;
+  int v = input->value;
+  // To increase the likelyhood of an incorrect calculation of a shared variable, we do it a lot!
+  for(int i = 0; i < 10000; i++){
+    temp++;
+  }
+  cout << "Thread #: " << thread_num << " value: " << v << " temp " << temp << endl;
+
+}
+
+
+const int NUM_THREADS = 3;
+int main(){
+
+  temp = 0;
+
+  thread_data_t passed[NUM_THREADS];
+  pthread_t threads[NUM_THREADS];
+
+  pthread_attr_t attr;
+  pthread_attr_init(&attr);
+  pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
+  for(int i = 0; i < NUM_THREADS; i++){
+    passed[i].thread_id = i;
+    passed[i].value = 55;
+    pthread_create(&threads[i], &attr, doStuff, (void*)&passed[i]);
+  }
+
+
+  for(int i = 0; i < NUM_THREADS; i++){
+    pthread_join(threads[i], NULL);
+    cout << "Thread " << i << " done." << endl;
+  }
+
+  cout << "temp (should be 30000): " << temp << endl;
+  pthread_attr_destroy(&attr);
+
+  return 0;
+
+}
