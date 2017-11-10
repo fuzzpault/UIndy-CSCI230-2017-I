17a689b634fa02fb6825b00db86deaf605d20f6e
Paul Talaga <talagap@uindy.edu>
2017-11-10 14:38:02 +0000

    Better comments on threading example

--- W11-Pthreads2/sumSquares.cpp
+++ W11-Pthreads2/sumSquares.cpp
@@ -1,28 +1,28 @@
 /*                                                                                                                     
   Name: Paul Talaga
   Date: Oct 30, 2017
-  Desc: Program to demonstrate the use of pthreads
-        This reads and writes to a shared variable with a lock around the critical 
-	section so no two threads update the variable at the same time.  This is 
-	SLOW as only one thread can execute their loop at a time, but at least it is
-	correct.  Bad threading implementation!
-
-  To compile this, do: g++ -lpthread thread1.cpp
+  Desc: This probram finds the sum of squares from 0 to 100000000,
+        it is used to verify the threaded fast version (sumSquaresFast) 
+        is correct.
+    
+        Compile: g++ -O3 sumSquares.cpp      (With full optimizations enabled)
+        Time: time ./a.out
 */
 
 #include <iostream>
-#include <pthread.h>
 
 using namespace std;
+
+const unsigned long MAX = 100000000;
 
 int main(){
     unsigned long long sum = 0;
 
-    for(unsigned long long i = 0; i <= 100000000; i++){
-      unsigned long long pre = sum;
+    for(unsigned long long i = 0; i <= MAX; i++){
       sum += i * i;
     }
     
+    cout << "Sum of squares from 0 to " << MAX << endl;
     cout << "Sum: " << sum << endl;
     
     return 0;
--- W11-Pthreads2/sumSquaresFast.cpp
+++ W11-Pthreads2/sumSquaresFast.cpp
@@ -3,9 +3,10 @@
   Date: Nov 7, 2017
   Desc: Program to demonstrate the use of pthreads
         This finds the sum of all the squares from 0 to a 'large number', using 
-	multiple threads. More here.
+	     multiple threads. More here.
 
-  To compile this, do: g++ -lpthread thread1.cpp
+      Compile: g++ -O3 sumSquaresFast.cpp -lpthread        (With full optimizations enabled)
+      Time: time ./a.out
 */
 
 #include <iostream>
@@ -14,41 +15,44 @@
 
 using namespace std;
 
+// Break the work up into many smaller batches, or range of numbers.  The only
+// way to pass parameters to a thread is via a pointer to a struct containing
+// the information, so the struct is defined here, with a start and ending values
+// included.  thread_id and value
+
 struct thread_data_t{
   int thread_id;
-  int value;
   unsigned long long start;
   unsigned long long end;
 };
 
-unsigned long long temp;
-pthread_mutex_t lock;
+unsigned long long total;   // Will store the total sum calculated by each thread.
+
+pthread_mutex_t lock;       // Global lock mutex so only 1 thread can modify total above.
 
 void* doStuff(void* arg){
   thread_data_t* input;
   input = (thread_data_t*)arg;
 
-  int thread_num = input->thread_id;
-  int v = input->value;
+  //  int thread_num = input->thread_id;  // Not used
   unsigned long long start = input->start;
   unsigned long long end = input->end;
-  //cout << start << " ->  " << end << endl;
   unsigned long long sum = 0;
   for(unsigned long long i = start; i <= end; i++){
     sum += i * i;
   }
-  pthread_mutex_lock(&lock);
-  temp += sum;
+  pthread_mutex_lock(&lock);    // Make sure only 1 thread is updating the shared variable total.
+  total += sum;
   pthread_mutex_unlock(&lock);
   
 }
 
 
-const int NUM_THREADS = 30;
+const int NUM_THREADS = 6;
 const unsigned long MAX = 100000000;
 int main(){
 
-  temp = 0;
+  total = 0;
 
   thread_data_t passed[NUM_THREADS];
   pthread_t threads[NUM_THREADS];
@@ -61,6 +65,8 @@ int main(){
 
   for(int i = 0; i < NUM_THREADS; i++){
     passed[i].thread_id = i;
+    // So we cover all the values, make all ranges equally-sized, except allow the 
+    // last to be large if there was a non-even division.
     if(i < NUM_THREADS -1){
       passed[i].start = (MAX / NUM_THREADS) * i + 1;
       passed[i].end = (MAX / NUM_THREADS) * (i + 1) ;
@@ -71,13 +77,13 @@ int main(){
     pthread_create(&threads[i], &attr, doStuff, (void*)&passed[i]);
   }
 
-
+  // Wait for all threads to be done.
   for(int i = 0; i < NUM_THREADS; i++){
     pthread_join(threads[i], NULL);
-    cout << "Thread " << i << " done." << endl;
+    // cout << "Thread " << i << " done." << endl;  // This slows it down, so don't print!
   }
 
-  cout << "sum is: " << temp << endl;
+  cout << "Sum is: " << total << endl;
   pthread_attr_destroy(&attr);
 
   pthread_mutex_destroy(&lock);
